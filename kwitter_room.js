65831d5c7a3dded3cf1d94255e7cbe3c490f16c1
Mega <83192893+MegaCoder2@users.noreply.github.com>
2021-11-18 18:58:52 -0600

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,20 @@
+
+const firebaseConfig = {
+      apiKey: "AIzaSyAMsl-x8qvZkb3RL2-zAPxMN9ZsOK7l4wQ",
+      authDomain: "kwitter-ccea5.firebaseapp.com",
+      databaseURL: "https://kwitter-ccea5-default-rtdb.firebaseio.com",
+      projectId: "kwitter-ccea5",
+      storageBucket: "kwitter-ccea5.appspot.com",
+      messagingSenderId: "312514588094",
+      appId: "1:312514588094:web:12d27023fe9f93d06d1def",
+      measurementId: "G-94BRTH17RR"
+    };
+firebase.initializeApp(firebaseConfig)
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
