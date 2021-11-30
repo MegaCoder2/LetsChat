dce67bdb8dc2ecbcfd2dfa8ed1a69278b558bba3
Mega <83192893+MegaCoder2@users.noreply.github.com>
2021-11-29 18:53:40 -0600

    Add files via upload

--- letschat_page.js
+++ letschat_page.js
@@ -11,6 +11,7 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig)
 var user = localStorage.getItem("Username")
 room = localStorage.getItem("room_name")
+var name = ""
 function logout()
 {
       localStorage.removeItem("Username")
@@ -26,4 +27,43 @@ function send()
         like: 0
     })
     document.getElementById("messsagebox").value = ""
+}
+    function getData() 
+    {
+          firebase.database().ref("/"+room).on('value', function(snapshot)
+           {
+                document.getElementById("output").innerHTML = "";
+                snapshot.forEach(function(childSnapshot) 
+                {
+                      childKey = childSnapshot.key;
+                      childData = childSnapshot.val();
+                      if(childKey != "purpose")
+                      {
+                      firebase_message_id = childKey
+                      message_data = childData
+                      console.log(firebase_message_id)
+                      console.log(message_data)
+                      Name = message_data["name"]
+                      message = message_data["message"]
+                      like = message_data["like"]
+                      namewithtag = "<h4>"+Name+"<img src='tick.png' class='user_tick'></h4>"
+                      messagewithtag = "<h4 class='message_h4'>"+message+"</h4>"
+                      buttonwithtag = "<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updatelike(this.id)'>"
+                      spanwithtag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+like+"</span></button><hr>"
+                      row = namewithtag + messagewithtag + buttonwithtag + spanwithtag
+                      document.getElementById("output").innerHTML += row
+                      console.log("row")
+                      }
+                })
+           })
+    }
+getData()
+function updatelike(message_id)
+{
+    button_id = message_id
+    likes = document.getElementById(button_id).value
+    updatelikes = Number(likes) + 1
+    firebase.database().ref(room).child(message_id).update({
+        like: updatelikes
+    })
 }
